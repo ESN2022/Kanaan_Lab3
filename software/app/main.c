e3a9a515839029b88a0362f3392d8f3bf8823e2e
Freddy <Kanaan>
2023-01-18 02:54:39 +0100

    Rewrote I2C read and write methods and added axis accelerations without correction

--- software/app/main.c
+++ software/app/main.c
@@ -1,50 +1,84 @@
-#include <stdio.h>
 #include "system.h"
+#include "alt_types.h"
 #include "opencores_i2c.h"
-#include "sys/alt_stdio.h"
+#include <sys/alt_stdio.h>
 
-#define ALT_ADDRESS 0x1D
-#define I2C_CLK_SPEED 100000
+#define ADXL345_I2C_ADDRESS 0x1D
+#define I2C_CLK_SPEED 100000u
+#define DATAX0 0X32 //	X-Axis Data 0 (LSB)
+#define DATAX1 0X33 //	X-Axis Data 1 (MSB)
+#define DATAY0 0X34 //	Y-Axis Data 0 (LSB)
+#define DATAY1 0X35 //	Y-Axis Data 1 (MSB)
+#define DATAZ0 0X36 //	Z-Axis Data 0 (LSB)
+#define DATAZ1 0X37 //	Z-Axis Data 1 (MSB)
 
+alt_16 x_axis = 0;
+alt_16 y_axis = 0;
+alt_16 z_axis = 0;
+alt_16 x_axis_offset = 0;
+alt_16 y_axis_offset = 0;
+alt_16 z_axis_offset = 0;
+int ack = 1;
 
-alt_16	x_axis	= 0;
-alt_16	y_axis	= 0;
-alt_16	z_axis	= 0;
-alt_u16 data	= 0;
-
-// start -> read value -> set axis
-void read_axis_values()
-{
-	I2C_start(OPENCORES_I2C_0_BASE, ALT_ADDRESS, 0);
-	I2C_write(OPENCORES_I2C_0_BASE, 0x33, 0);
-	
-	data = I2C_read(OPENCORES_I2C_0_BASE, 1);
-		
-}
-
-void display_axis()
-{
-	
-	printf("x=%d\ty=%d\tz=%d\r\n", x_axis, y_axis, z_axis);
-}
+alt_u32 I2C_READ_ADXL345(alt_u8 reg);
+void I2C_WRITE_ADXL345(alt_u8 reg, alt_u8 data);
 
 int main()
 {
-	I2C_init(OPENCORES_I2C_0_BASE, ALT_CPU_FREQ, I2C_CLK_SPEED);
-	if( I2C_start(OPENCORES_I2C_0_BASE, ALT_ADDRESS, 0) == 0 )
+	I2C_init(ADXL345_I2C_ADDRESS, ALT_CPU_CPU_FREQ, I2C_CLK_SPEED);
+	while (1)
 	{
-		printf("I2C success\r\n");
+		x_axis = (alt_16)((I2C_READ_ADXL345(DATAX1) << 8) | I2C_READ_ADXL345(DATAX0));
+		alt_printf("%d\t", x_axis);
+		y_axis = (alt_16)((I2C_READ_ADXL345(DATAY1) << 8) | I2C_READ_ADXL345(DATAY0));
+		alt_printf("%d\t", y_axis);
+		z_axis = (alt_16)((I2C_READ_ADXL345(DATAZ1) << 8) | I2C_READ_ADXL345(DATAZ0));
+		alt_printf("%d\n", z_axis);
 
-	}
-	else
-	{
-		printf("I2C failure\r\n");
-	}
-		data = I2C_read(OPENCORES_I2C_0_BASE, 1);
-	 I2C_write(OPENCORES_I2C_0_BASE, ALT_ADDRESS, 1);
-	 alt_printf();
-	while(1){
-		display_axis();
+		usleep(200000);
 	}
 }
-	
+
+alt_u32 I2C_READ_ADXL345(alt_u8 reg)
+{
+	alt_u32 read_byte;
+
+	while (ack)
+	{
+		ack = I2C_start(ADXL345_I2C_ADDRESS, ADXL345_I2C_ADDRESS, 0);
+		if (ack == 0)
+		{
+			ack = 1;
+			break;
+		}
+	}
+	I2C_write(OPENCORES_I2C_0_BASE, reg, 0);
+
+	while (ack)
+	{
+		ack = I2C_start(ADXL345_I2C_ADDRESS, ADXL345_I2C_ADDRESS, 1);
+		if (ack == 0)
+		{
+			ack = 1;
+			break;
+		}
+	}
+	read_byte = I2C_read(OPENCORES_I2C_0_BASE, 1);
+
+	return read_byte;
+}
+
+void I2C_WRITE_ADXL345(alt_u8 reg, alt_u8 data)
+{
+	while (ack)
+	{
+		ack = I2C_start(ADXL345_I2C_ADDRESS, ADXL345_I2C_ADDRESS, 0);
+		if (ack == 0)
+		{
+			ack = 1;
+			break;
+		}
+	}
+	I2C_write(OPENCORES_I2C_0_BASE, reg, 0);
+	I2C_write(OPENCORES_I2C_0_BASE, data, 1);
+}
